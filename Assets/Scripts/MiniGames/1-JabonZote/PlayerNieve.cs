76ea24c76b44801cff23f69fa6bf588160ea4b05
HugoVD <sartos3000@hotmail.es>
2025-01-24 02:20:36 -0600

    UpdateJuegojabon

--- Assets/Scripts/MiniGames/1-JabonZote/PlayerNieve.cs
+++ Assets/Scripts/MiniGames/1-JabonZote/PlayerNieve.cs
@@ -4,15 +4,23 @@ using UnityEngine.EventSystems;
 
 public class PlayerNieve : MinigamesBase
 {
-    public List<Vector3> figurePoints; // Lista de puntos que definen la figura
-    private int currentPointIndex = 0;
+    [Header("Figuras")]
+    public List<Vector3> squarePoints;
+    public List<Vector3> circlePoints;
+    public List<Vector3> trianglePoints;
+    private List<Vector3> figurePoints;
+
+    [SerializeField] private int currentPointIndex = 0;
     [SerializeField] private bool isDrawing = false;
-    [SerializeField] private float tolerance = 0.5f;
+    [SerializeField] private float tolerance = 1f;
+    [SerializeField] private LineRenderer lineRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SelectRandomFigure();
+        DrawPath();
+        TeleportToStart();
     }
 
     // Update is called once per frame
@@ -37,52 +45,99 @@ public class PlayerNieve : MinigamesBase
 
     void MovePlayer()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.y = 0; // Asegúrate de que el jugador se mueva solo en el plano XZ
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.transform.position.y;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.y = 0; // Asegúrate de que el jugador se mueva solo en el plano XZ
 
-        transform.position = new Vector3(mousePosition.x, 0, mousePosition.z);
+        transform.position = new Vector3(worldPosition.x, 0, worldPosition.z);
 
-        if (Vector3.Distance(transform.position, figurePoints[currentPointIndex]) < 0.1f)
+        Debug.Log("Posición del jugador: " + transform.position);
+        Debug.Log("Posición del punto actual: " + figurePoints[currentPointIndex]);
+
+        if (Vector3.Distance(transform.position, figurePoints[currentPointIndex]) < tolerance)
         {
+            Debug.Log("Punto alcanzado: " + currentPointIndex);
             currentPointIndex++;
             if (currentPointIndex >= figurePoints.Count)
             {
-                currentPointIndex = 0;
+                Debug.Log("Todos los puntos alcanzados!");
+                isDrawing = false;
+                CheckCompletion();
             }
         }
-        else if (!IsWithinTolerance(mousePosition))
-        {
-            Debug.Log("Te has salido de los límites!");
-            isDrawing = false;
-            currentPointIndex = 0; // Reinicia el índice de puntos
-        }
-    }
-
-    bool IsWithinTolerance(Vector3 position)
-    {
-        foreach (var point in figurePoints)
-        {
-            if (Vector3.Distance(position, point) <= tolerance)
-            {
-                return true;
-            }
-        }
-        return false;
     }
 
     void CheckCompletion()
     {
-        if (currentPointIndex == figurePoints.Count - 1)
+        if (currentPointIndex == figurePoints.Count)
         {
             Debug.Log("Figura completada!");
             // Lógica para cuando el jugador completa la figura
+            GameManager.instance.CompleteMinigame();
         }
         else
         {
             Debug.Log("Figura no completada. Inténtalo de nuevo.");
             // Lógica para cuando el jugador no completa la figura
+            GameManager.instance.FailMinigame();
         }
 
         currentPointIndex = 0; // Reinicia el índice de puntos
+    }
+
+    void SelectRandomFigure()
+    {
+        int randomFigure = Random.Range(0, 3); // Selecciona un número al azar entre 0 y 2
+        if (randomFigure == 0)
+        {
+            figurePoints = squarePoints;
+            Debug.Log("Figura seleccionada: Cuadrado");
+        }
+        else if (randomFigure == 1)
+        {
+            figurePoints = circlePoints;
+            Debug.Log("Figura seleccionada: Círculo");
+        }
+        else
+        {
+            figurePoints = trianglePoints;
+            Debug.Log("Figura seleccionada: Triángulo");
+        }
+
+        Debug.Log("Puntos de la figura seleccionada: " + figurePoints.Count);
+    }
+
+    void DrawPath()
+    {
+        if (lineRenderer != null && figurePoints != null)
+        {
+            List<Vector3> pointsToDraw = new List<Vector3>(figurePoints);
+            if (pointsToDraw.Count > 0)
+            {
+                pointsToDraw.Add(pointsToDraw[0]); // Añadir el primer punto al final para cerrar el bucle
+            }
+            lineRenderer.positionCount = pointsToDraw.Count;
+            lineRenderer.SetPositions(pointsToDraw.ToArray());
+        }
+
+    }
+
+    void TeleportToStart()
+    {
+        if (figurePoints != null && figurePoints.Count > 0)
+        {
+            Vector3 startPoint = figurePoints[0];
+            transform.position = new Vector3(startPoint.x, 0, startPoint.z);
+        }
+    }
+
+    public override void ResetMinigame()
+    {
+        currentPointIndex = 0;
+        isDrawing = false;
+        SelectRandomFigure();
+        DrawPath();
+        TeleportToStart();
     }
 }
