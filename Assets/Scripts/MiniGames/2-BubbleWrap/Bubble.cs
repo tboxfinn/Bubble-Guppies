d6d4392432b4764b87fa4455ce368c26a0a8b860
HugoVD <sartos3000@hotmail.es>
2025-01-24 12:12:04 -0600

    BubbleWrap Game in progress

---
+++ Assets/Scripts/MiniGames/2-BubbleWrap/Bubble.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class Bubble : MonoBehaviour, IPointerClickHandler
+{
+    public bool isPopped = false;
+    public GameObject inflatedBubble;
+    public GameObject poppedBubble;
+
+    public event Action OnBubblePopped;
+
+    void Start()
+    {
+        UpdateBubble();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isPopped)
+        {
+            isPopped = true;
+            UpdateBubble();
+            OnBubblePopped?.Invoke();
+            // Lógica adicional para cuando una burbuja se revienta
+        }
+    }
+
+    void UpdateBubble()
+    {
+        if (isPopped)
+        {
+            inflatedBubble.SetActive(false);
+            poppedBubble.SetActive(true);
+        }
+        else
+        {
+            inflatedBubble.SetActive(true);
+            poppedBubble.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/MiniGames/2-BubbleWrap/WrapPaper.cs
+++ Assets/Scripts/MiniGames/2-BubbleWrap/WrapPaper.cs
@@ -1,16 +1,74 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class WrapPaper : MonoBehaviour
+public class WrapPaper : MinigamesBase
 {
+    public GameObject bubblePrefab;
+    public int rows = 5;
+    public int columns = 5;
+    public float spacing = 1.5f;
+
+    private List<Bubble> bubbles = new List<Bubble>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GenerateBubbles();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateBubbles()
     {
-        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector3 position = new Vector3(i * spacing, j * spacing, 0);
+                GameObject bubbleObject = Instantiate(bubblePrefab, position, Quaternion.identity);
+                bubbleObject.transform.SetParent(transform);
+
+                Bubble bubble = bubbleObject.GetComponent<Bubble>();
+                if (bubble != null)
+                {
+                    bubble.OnBubblePopped += HandleBubblePopped;
+                    bubbles.Add(bubble);
+                }
+            }
+        }
+    }
+
+    void HandleBubblePopped()
+    {
+        if (AllBubblesPopped())
+        {
+            GameManager.instance.CompleteMinigame();
+        }
+    }
+
+    bool AllBubblesPopped()
+    {
+        foreach (Bubble bubble in bubbles)
+        {
+            if (!bubble.isPopped)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public override void ResetMinigame()
+    {
+        // Destruir burbujas existentes
+        foreach (Bubble bubble in bubbles)
+        {
+            if (bubble != null)
+            {
+                Destroy(bubble.gameObject);
+            }
+        }
+        bubbles.Clear();
+
+        // Generar nuevas burbujas
+        GenerateBubbles();
     }
 }
