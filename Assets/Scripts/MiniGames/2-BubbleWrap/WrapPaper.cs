9edfa46623658956b568cc4addc947ba9db0838d
HugoVD <sartos3000@hotmail.es>
2025-01-24 22:46:21 -0600

    Tapioca y BurbujaPapel

--- Assets/Scripts/MiniGames/2-BubbleWrap/WrapPaper.cs
+++ Assets/Scripts/MiniGames/2-BubbleWrap/WrapPaper.cs
@@ -6,9 +6,14 @@ public class WrapPaper : MinigamesBase
     public GameObject bubblePrefab;
     public int rows = 5;
     public int columns = 5;
-    public float spacing = 1.5f;
+    public float spacingX = 1.5f;
+    public float spacingY = 1.5f;
+    public float initialPoppedProbability = 0.9f; // Probabilidad de que una burbuja aparezca reventada
+    public float difficultyIncreaseRate = 0.05f;
+    public float minPoppedProbability = 0.4f;
 
     private List<Bubble> bubbles = new List<Bubble>();
+    private int bubblesToPop;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,17 +23,32 @@ public class WrapPaper : MinigamesBase
 
     void GenerateBubbles()
     {
+        float startX = -(columns - 1) * spacingX / 2;
+        float startY = -(rows - 1) * spacingY / 2;
+
+        bubblesToPop = 0;
+
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
-                Vector3 position = new Vector3(i * spacing, j * spacing, 0);
-                GameObject bubbleObject = Instantiate(bubblePrefab, position, Quaternion.identity);
-                bubbleObject.transform.SetParent(transform);
+                Vector3 position = new Vector3(startX + j * spacingX, startY + i * spacingY, 0);
+                GameObject bubbleObject = Instantiate(bubblePrefab, transform);
+                bubbleObject.transform.localPosition = position;
+                bubbleObject.transform.localRotation = Quaternion.identity;
 
                 Bubble bubble = bubbleObject.GetComponent<Bubble>();
                 if (bubble != null)
                 {
+                    // Establecer aleatoriamente el estado de la burbuja
+                    bubble.isPopped = Random.value < initialPoppedProbability;
+                    bubble.UpdateBubble();
+
+                    if (!bubble.isPopped)
+                    {
+                        bubblesToPop++;
+                    }
+
                     bubble.OnBubblePopped += HandleBubblePopped;
                     bubbles.Add(bubble);
                 }
@@ -38,22 +58,18 @@ public class WrapPaper : MinigamesBase
 
     void HandleBubblePopped()
     {
-        if (AllBubblesPopped())
+        bubblesToPop--;
+
+        if (bubblesToPop <= 0)
         {
+            IncreaseDifficulty();
             GameManager.instance.CompleteMinigame();
         }
     }
 
-    bool AllBubblesPopped()
+    void IncreaseDifficulty()
     {
-        foreach (Bubble bubble in bubbles)
-        {
-            if (!bubble.isPopped)
-            {
-                return false;
-            }
-        }
-        return true;
+        initialPoppedProbability = Mathf.Max(minPoppedProbability, initialPoppedProbability - difficultyIncreaseRate);
     }
 
     public override void ResetMinigame()
@@ -71,4 +87,6 @@ public class WrapPaper : MinigamesBase
         // Generar nuevas burbujas
         GenerateBubbles();
     }
+
+    
 }
